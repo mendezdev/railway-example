f1ea402b1dbe2e8b7edadecd77bd4fad4be51d0b
Pablo <mendezdev@users.noreply.github.com>
2024-04-29 20:25:45 -0300

    added first main file

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	fmt.Println("starting....")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/appetizers", func(w http.ResponseWriter, r *http.Request) {
+		url := "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/appetizers"
+		res, err := http.Get(url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			msg := fmt.Sprintf("error sending request: %s", err.Error())
+			w.Write([]byte(msg))
+			return
+		}
+
+		resBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			msg := fmt.Sprintf("error reading body response: %s", err.Error())
+			w.Write([]byte(msg))
+			return
+		}
+
+		fmt.Printf("RESPONSE: %s\n", string(resBody))
+
+		w.Header().Add("Content-Type", "application/json")
+
+		w.Write(resBody)
+	})
+	http.ListenAndServe(":8080", r)
+}
